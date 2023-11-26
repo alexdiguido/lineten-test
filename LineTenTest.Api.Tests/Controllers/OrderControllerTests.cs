34a71289de301fb940198559ea6213e0238e0387
AzureAD\AlessandroDiGuido <alessandro.di-guido@mmtdigital.co.uk>
2023-11-26 12:14:29 +0000

    add api order controller with first tests.

---
+++ LineTenTest.Api.Tests/Controllers/OrderControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using LineTenTest.Api.Controllers;
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace LineTenTest.Api.Tests.Controllers
+{
+    public class OrderControllerTests
+    {
+        [Fact]
+        public async Task Get_OrderIdValid_ShouldReturnOkActionResult()
+        {
+            // Arrange
+            var orderController = new OrderController();
+
+            // Act
+            int orderId = 1;
+
+            var result = await orderController.Get(orderId);
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+    }
+}
---
+++ LineTenTest.Api/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LineTenTest.Api.Controllers
+{
+    [ApiController]
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]/[action]")]
+    public class OrderController : Controller
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Get(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
