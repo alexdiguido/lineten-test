f209a099c7127f04d9e689eda5bc2572f2b61108
AzureAD\AlessandroDiGuido <alessandro.di-guido@mmtdigital.co.uk>
2023-11-26 13:07:49 +0000

    add test for the scenario when mediator.send throws an exception. It may depend for the purpose of the api but it may be not good practice to return internal information to the clients.

--- LineTenTest.Api.Tests/Controllers/OrderControllerTests.cs
+++ LineTenTest.Api.Tests/Controllers/OrderControllerTests.cs
@@ -45,5 +45,31 @@ namespace LineTenTest.Api.Tests.Controllers
             result.Should().NotBeNull();
             _mockRepository.VerifyAll();
         }
+
+        [Fact]
+        public async Task Get_MediatorThrowsException_ShouldReturnInternalServerErrorActionResult()
+        {
+            // Arrange
+            int orderId = 1;
+            var orderController = CreateService();
+            var expectedDto = new OrderDto
+            {
+                OrderId = orderId
+            };
+            var expectedStatusCode = 500;
+            var expectedMessage = $"an error occurred. Please contact Application admin";
+
+            _mockMediator.Setup(expression: m => m.Send(It.Is<GetByOrderIdQuery>(q=> q.OrderId == orderId), It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new Exception("exception message with internal stacktrace"));
+            // Act
+            var result = await orderController.Get(orderId);
+
+            var objectResult = result.Result as ObjectResult;
+            // Assert
+            
+            objectResult.StatusCode.Should().Be(expectedStatusCode);
+            objectResult.Value.Should().Be(expectedMessage);
+            _mockRepository.VerifyAll();
+        }
     }
 }
