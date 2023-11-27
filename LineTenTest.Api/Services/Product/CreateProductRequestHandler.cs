8da0f8a6f34337cabe4b00b6d5c187431f540135
AzureAD\AlessandroDiGuido <alessandro.di-guido@mmtdigital.co.uk>
2023-11-27 17:31:01 +0000

    add create product request handler implementation.

--- LineTenTest.Api/Services/Product/CreateProductRequestHandler.cs
+++ LineTenTest.Api/Services/Product/CreateProductRequestHandler.cs
@@ -1,8 +1,13 @@
-﻿using LineTenTest.Api.Commands;
+﻿using System.Net;
+using Ardalis.GuardClauses;
+using LineTenTest.Api.Commands;
 using LineTenTest.Api.Dtos;
+using LineTenTest.Api.Utilities;
+using LineTenTest.Api.Utilities.Mappers;
 using LineTenTest.Domain.Services.Product;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using NotFoundException = LineTenTest.Domain.Exceptions.NotFoundException;
 
 namespace LineTenTest.Api.Services.Product
 {
@@ -17,9 +22,35 @@ namespace LineTenTest.Api.Services.Product
             _logger = logger;
         }
 
-        public Task<ActionResult<ProductDto>> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<ActionResult<ProductDto>> Handle(CreateProductCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            Domain.Entities.Product? productResult;
+            try
+            {
+                Guard.Against.Null(request.Request);
+                Guard.Against.NullOrEmpty(request.Request.Description);
+                Guard.Against.NullOrEmpty(request.Request.Name);
+                Guard.Against.NullOrEmpty(request.Request.Sku);
+                productResult = await _service.CreateAsync(request.Request);
+            }
+            catch (ArgumentException argumentEx)
+            {
+                return new BadRequestObjectResult(argumentEx.Message);
+            }
+            catch (NotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, request);
+                return new ObjectResult(Constants.InternalServerErrorResultMessage)
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError
+                };
+            }
+
+            return new OkObjectResult(ProductDtoMapper.MapFrom(productResult));
         }
     }
 }
