db6c515c5e48e5f511eece17ae8e7f9620f8f8ee
AzureAD\AlessandroDiGuido <alessandro.di-guido@mmtdigital.co.uk>
2023-11-28 10:22:06 +0000

    add delete request handler implementation.

--- LineTenTest.Api/Services/Customer/DeleteCustomerRequestHandler.cs
+++ LineTenTest.Api/Services/Customer/DeleteCustomerRequestHandler.cs
@@ -1,19 +1,46 @@
-﻿using LineTenTest.Api.Commands;
+﻿using Ardalis.GuardClauses;
+using LineTenTest.Api.Commands;
 using LineTenTest.Domain.Services.Customer;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using LineTenTest.Api.Utilities;
 
 namespace LineTenTest.Api.Services.Customer
 {
     public class DeleteCustomerRequestHandler : IRequestHandler<DeleteCustomerCommand,IActionResult>
     {
+        private readonly IDeleteCustomerService _service;
+        private readonly ILogger<DeleteCustomerRequestHandler> _logger;
+
         public DeleteCustomerRequestHandler(IDeleteCustomerService service, ILogger<DeleteCustomerRequestHandler> logger)
         {
-            
+            _service = service;
+            _logger = logger;
         }
-        public Task<IActionResult> Handle(DeleteCustomerCommand request, CancellationToken cancellationToken)
+
+        public async Task<IActionResult> Handle(DeleteCustomerCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            Domain.Entities.Customer? customerResult;
+            try
+            {
+                Guard.Against.Negative(request.Request.CustomerId);
+                await _service.DeleteAsync(request.Request);
+            }
+            catch (ArgumentException argumentEx)
+            {
+                return new BadRequestObjectResult(argumentEx.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, request);
+                return new ObjectResult(Constants.InternalServerErrorResultMessage)
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError
+                };
+            }
+
+            return new OkResult();
         }
     }
 }
